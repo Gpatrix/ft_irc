fe89baac39480d095896776083be13f4e01c7102
lchauvet <lchauvet@student.42lehavre.fr>
2025-03-03 12:58:54 +0100

    add|WIP: bind

--- include/server.hpp
+++ include/server.hpp
@@ -14,6 +14,7 @@
 #include <sys/types.h>
 
 #include <cstring>
+#include <cstdlib>
 
 class server
 {
--- src/server/server.cpp
+++ src/server/server.cpp
@@ -9,31 +9,26 @@ server::server(char* port, char* password)
 
 	this->sockfd = socket(AF_INET6, SOCK_STREAM | SOCK_NONBLOCK, 0);
 	if (this->sockfd < 0)
-	{
 		throw std::runtime_error("error: init socket");
-	}
 
 	rc = setsockopt(this->sockfd, IPPROTO_TCP, SO_REUSEADDR | SO_REUSEPORT, NULL, 0);
 	if (rc < 0)
-	{
 		throw std::runtime_error("error: setsockopt() failed");
-	}
 	struct sockaddr_in6   addr;
 	
 	memset(&addr, 0, sizeof(addr));
 	addr.sin6_family      = AF_INET6;
-
 	memcpy(&addr.sin6_addr, &in6addr_any, sizeof(in6addr_any));
 
-	// TODO parse port
-	addr.sin6_port = htons(SERVER_PORT);
+	char*	end;
+	uint16_t	port_nb = static_cast<uint16_t>(strtol(port, &end, 10));
+	if (*end != '\0')
+		throw std::runtime_error("bad port");
+	
+	addr.sin6_port = htons(port_nb);
 	rc = bind(this->sockfd, (struct sockaddr *)&addr, sizeof(addr));
 	if (rc < 0)
-	{
 		throw std::runtime_error("error: bind() failed");
-	}
-
-
 }
 
 server::~server(void) {}
