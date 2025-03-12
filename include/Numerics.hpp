a9fa646797a04a703468f96061b0262b3f873847
Gap <132090220+Gpatrix@users.noreply.github.com>
2025-03-12 12:50:56 +0100

    fix: Numerics order

--- include/Numerics.hpp
+++ include/Numerics.hpp
@@ -20,10 +20,8 @@ class Numerics
 		static void _401_ERR_NOSUCHNICK(const std::string& nick, const int& fd);
 		static void _403_ERR_NOSUCHCHANNEL(const std::string& channel, const int& fd);
 		static void _404_ERR_CANNOTSENDTOCHAN(const std::string& channel, const int& fd);
-
-		static void _412_ERR_NOTEXTTOSEND(const int& fd);
-
 		static void _405_ERR_TOOMANYCHANNELS(const std::string& client, const std::string& channel, const int& fd);
+		static void _412_ERR_NOTEXTTOSEND(const int& fd);
 		static void _421_ERR_UNKNOWNCOMMAND(const std::string& cmd, const int& fd);
 		static void _432_ERR_ERRONEUSNICKNAME(const std::string& client, const std::string& nickname, const int& fd);
 		static void _433_ERR_NICKNAMEINUSE(const std::string& new_nickname, const int& fd);
